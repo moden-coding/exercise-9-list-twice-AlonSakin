0ed33ef732def7a47c44cea74dc54b87dfceadee
AlonSakin <29asakin@gannacademy.org>
2025-09-02 15:43:07 -0400

    done

--- src/list_twice.py
+++ src/list_twice.py
@@ -1,3 +1,7 @@
-# Write your solution here
-list = []
+l = []
 
+while n := int(input("New item: ")):
+    l.append(n)
+    print(f"The list now: {l}")
+    print(f"The list in order: {sorted(l)}")
+print("Bye!")
